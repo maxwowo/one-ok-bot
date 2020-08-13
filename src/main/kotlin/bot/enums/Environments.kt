c1dd4906c2e4dec7d6aca78f576c26e98bc5da7a
Max Wo <maxwo@protonmail.com>
2020-08-14 02:03:30 +1200

    Some comments

--- src/main/kotlin/bot/enums/Environments.kt
+++ src/main/kotlin/bot/enums/Environments.kt
@@ -1,5 +1,10 @@
 package bot.enums
 
+/**
+ * An enum representing each type of environment the project can be in
+ *
+ * @param value The corresponding value of the environment
+ */
 enum class Environments(val value: String) {
     DEVELOPMENT("DEVELOPMENT"),
     TEST("TEST"),
--- src/main/kotlin/bot/enums/Variables.kt
+++ src/main/kotlin/bot/enums/Variables.kt
@@ -1,5 +1,10 @@
 package bot.enums
 
+/**
+ * An enum which stores the values of all the environment variables needed
+ *
+ * @param value The corresponding value of the environment variable
+ */
 enum class Variables(val value: String) {
     ENVIRONMENT("ENVIRONMENT")
 }
