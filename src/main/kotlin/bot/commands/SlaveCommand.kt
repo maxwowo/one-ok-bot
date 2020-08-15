a7f535d9f121bdc380e2dcf3969388c65d08b563
Max Wo <maxwo@protonmail.com>
2020-08-15 23:43:28 +1200

    Lint

--- src/main/kotlin/bot/commands/SlaveCommand.kt
+++ src/main/kotlin/bot/commands/SlaveCommand.kt
@@ -2,7 +2,6 @@ package bot.commands
 
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
-import net.dv8tion.jda.api.EmbedBuilder
 
 /**
  * Class that represents !slave command
