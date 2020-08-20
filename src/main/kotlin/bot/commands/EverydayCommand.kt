68b87d4f9c6e5576893be4316ef93a9698c99316
Max Wo <maxwo@protonmail.com>
2020-08-20 19:47:09 +1200

    Everyday command error checking

--- src/main/kotlin/bot/commands/EverydayCommand.kt
+++ src/main/kotlin/bot/commands/EverydayCommand.kt
@@ -4,6 +4,7 @@ import bot.sounds.audioPlayerManager
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandEvent
 import net.dv8tion.jda.api.EmbedBuilder
+import java.awt.Color
 
 /**
  * Class that represents the !everyday command
@@ -17,15 +18,36 @@ class EverydayCommand : Command() {
     }
 
     override fun execute(event: CommandEvent) {
-        val builder = EmbedBuilder()
         val musicManager = audioPlayerManager.findGuildMusicManager(event.guild)
+        val scheduler = musicManager.scheduler
 
-        builder.setDescription("I'm shufflin'")
-        builder.setFooter(event.author.name)
-        builder.setTimestamp(event.message.timeCreated)
+        if (event.guild.audioManager.isConnected) {
+            if (scheduler.isEmpty()) {
+                val builder = EmbedBuilder()
 
-        musicManager.scheduler.shuffle()
+                builder.setDescription("${event.author.asMention} Bro the queue is empty")
+                builder.setColor(Color.RED)
+                builder.setTimestamp(event.message.timeCreated)
 
-        event.reply(builder.build())
+                event.reply(builder.build())
+            } else {
+                val builder = EmbedBuilder()
+
+                builder.setDescription("I'm shufflin'")
+                builder.setFooter(event.author.name)
+                builder.setTimestamp(event.message.timeCreated)
+
+                musicManager.scheduler.shuffle()
+
+                event.reply(builder.build())
+            }
+        } else {
+            val builder = EmbedBuilder()
+
+            builder.setDescription("${event.author.asMention} Bro I'm not even in a voice channel")
+            builder.setColor(Color.RED)
+
+            event.reply(builder.build())
+        }
     }
 }
